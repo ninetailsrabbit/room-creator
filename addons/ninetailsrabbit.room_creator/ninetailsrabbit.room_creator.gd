c13f040795e2ace09895cbd3e782c4375aa76d36
ninetailsrabbit <danieldigitalspace@proton.me>
2025-04-01 12:57:42 +0100

    use absolute paths on the load

--- addons/ninetailsrabbit.room_creator/ninetailsrabbit.room_creator.gd
+++ addons/ninetailsrabbit.room_creator/ninetailsrabbit.room_creator.gd
@@ -4,10 +4,20 @@ extends EditorPlugin
 var inspector_plugin
 
 func _enter_tree() -> void:
-	add_custom_type("RoomCreator", "Node3D", preload("src/room_creator.gd"), preload("assets/icon.svg"))
-	add_custom_type("DungeonGenerator", "Node3D", preload("src/dungeon/dungeon_generator.gd"), preload("assets/icon.svg"))
+	add_custom_type(
+		"RoomCreator",
+		"Node3D",
+		preload("res://addons/ninetailsrabbit.room_creator/src/room_creator.gd"),
+		preload("res://addons/ninetailsrabbit.room_creator/assets/icon.svg")
+	)
+	add_custom_type(
+		"DungeonGenerator",
+		"Node3D",
+		preload("res://addons/ninetailsrabbit.room_creator/src/dungeon/dungeon_generator.gd"),
+		preload("res://addons/ninetailsrabbit.room_creator/assets/icon.svg")
+	)
 	
-	inspector_plugin = preload("src/inspector/inspector_button_plugin.gd").new()
+	inspector_plugin = preload("res://addons/ninetailsrabbit.room_creator/src/inspector/inspector_button_plugin.gd").new()
 	add_inspector_plugin(inspector_plugin)
 
 
